804ddac7b5f66580d32aa7790155359329e9828a
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-07 13:21:03 +0800

    Contoh StatefulWidget
    Ada dua jenis widget utama dalam Flutter:
    
    StatelessWidget - Widget ini tidak memiliki kondisi yang berubah. Sekali dibuat, widget ini tidak akan berubah seiring waktu atau interaksi pengguna. Contohnya, widget teks atau gambar yang statis.
    
    StatefulWidget - Widget ini memiliki kondisi (state) yang bisa berubah. Widget ini cocok untuk elemen yang interaktif atau yang perlu diperbarui, seperti formulir input atau daftar yang bisa digulir. StatefulWidget memiliki dua komponen, yaitu widget itu sendiri dan state-nya, yang bisa berubah seiring waktu.

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,9 +12,7 @@ class MyApp extends StatelessWidget {
       ),
       home: const Scaffold(
         body: Center(
-          child: Heading(
-          text: "Hello world!", // text biasa
-          ),
+          child: BiggerText(text:"Hello world!"), // Ubah widget Heading ke PerubahanText
         ),
       ),
     );
@@ -24,12 +20,12 @@ class MyApp extends StatelessWidget {
 }
 
 class Heading extends StatelessWidget {
-  final String text; // state text bersifat final,  Nilainya hanya dapat diisi melalui constructor class-nya.
+  final String text;
 
-  const Heading({Key? key, required this.text}) : super(key: key); // lalu state text masuk ke constructor
+  const Heading({Key? key, required this.text}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Text(
       text,
       style: const TextStyle(
@@ -40,3 +36,29 @@ class Heading extends StatelessWidget {
   }
 }
 
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+  @override
+  _BiggerTextState createState() => _BiggerTextState();
+}
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+          child: const Text("Perbesar"),
+          onPressed: () {
+            setState(() {
+              _textSize = 32.0;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
