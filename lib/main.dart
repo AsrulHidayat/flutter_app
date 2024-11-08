1209280f3aef273b22bdcbbf47266ddf6b0d914c
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-08 08:45:58 +0800

    Belajar Scaffold
    Scaffold yang terdiri atas AppBar, Body, dan FloatingActionButton yang merupakan kerangka atau widget penting dalam flutter, sebenarnya masih banyak tapi 3 komponen ini adalah yang paling utama.

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+        )
       ),
-      home: const Scaffold(
-        body: Center(
-          child: BiggerText(text:"Hello world!"), // Ubah widget Heading ke PerubahanText
+      home: const FirstScreen(),// Panggil FirstScreen di sini
+
+    );
+  }
+}
+
+// Sekarang kita akan membuat scaffold yang terdiri atas AppBar, Body, dan FloatingActionButton yang merupakan kerangka atau widget penting dalam flutter, sebenarnya masih banyak tapi 3 komponen ini adalah yang paling utama.
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( // Membuat AppBar yang berada paling atas aplikasi
+        title: const Text('First Screen'),
+        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {},
         ),
       ),
-    );
-  }
-}
-
-class Heading extends StatelessWidget {
-  final String text;
-
-  const Heading({Key? key, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-        fontSize: 24.0,
-        fontWeight: FontWeight.bold,
+      body: const Center( // Penambahan Body, Badan Aplikasi
+        child: Text('Hello world'),
       ),
-    );
-  }
-}
-
-class BiggerText extends StatefulWidget {
-  final String text;
-  const BiggerText({Key? key, required this.text}) : super(key: key);
-  @override
-  _BiggerTextState createState() => _BiggerTextState();
-}
-class _BiggerTextState extends State<BiggerText> {
-  double _textSize = 16.0;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(widget.text, style: TextStyle(fontSize: _textSize)),
-        ElevatedButton(
-          child: const Text("Perbesar"),
-          onPressed: () {
-            setState(() {
-              _textSize = 32.0;
-            });
-          },
-        )
-      ],
+      floatingActionButton: FloatingActionButton( // ingat awalan huruf kecil atau camelCase menandakan kalau itu adalah property. Sedangkan awalan kapital menandakan bahwa itu adalah Class.
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
