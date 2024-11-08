85e8ccbcbacc10f62897766f6c5a1c57256bd59e
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-08 14:46:41 +0800

    Create : Description Aplikasi Tempat Wisata

--- lib/main.dart
+++ lib/main.dart
@@ -8,105 +8,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.blue,
-        ),
-      ),
-      home: const FirstScreen(), // Panggil FirstScreen di sini
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: const DetailScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({Key? key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-        titleTextStyle: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ],
-        leading: IconButton(
-          icon: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-      ),
-      body: Column( // Menggunakan Column untuk menempatkan widget secara vertikal
-        mainAxisAlignment: MainAxisAlignment.start, // Menyusun secara vertikal ke atas
+      body: SafeArea(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-
-          Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row( // Row di atas Container
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Menyusun item di dalam Row ke tengah
-                children: const <Widget>[
-                  Icon(Icons.share, color: Colors.blue),
-                  Icon(Icons.thumb_up, color: Colors.blue),
-                  Icon(Icons.thumb_down,color: Colors.blue),
-              ],
-            ),
-          ),
-
-          const SizedBox(height: 20), // Memberikan jarak antara Row dan Container
-
-          // Container yang ada sebelumnya
-          Container(
-            decoration: BoxDecoration( // Gunakan BoxDecoration untuk styling
-              color: Colors.red, // Tentukan warna dalam BoxDecoration
-              border: Border.all(color: Colors.orange, width: 3),
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.black,
-                  offset: Offset(3, 6),
-                  blurRadius: 10,
-                ),
-              ],
-            ),
-            margin: const EdgeInsets.all(10),
-            padding: const EdgeInsets.all(30),
-            child: const Text(
-              'Hi',
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.white,
-              ),
-            ),
-          ),
-
-          const SizedBox(height: 20), // Memberikan jarak antara Container dan Row berikutnya
-
-          Column(
-            children: const <Widget>[
-              Text(
-                'Sebuah Judul',
-                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              ),
-              Text('Lorem ipsum dolor sit amet'),
+         Container(
+           margin: const EdgeInsets.all(16.0),
+           child: Text(
+             'Malino Highland',
+             textAlign: TextAlign.center,
+             style: TextStyle(
+               fontSize: 30.0,
+               fontWeight: FontWeight.bold,
+             ),
+           )
+        ),
+         Container(
+          margin: EdgeInsets.symmetric(vertical: 16.0),
+           child: Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               Column(
+                   children: const <Widget>[
+                   Icon(Icons.calendar_today),
+                   SizedBox(height: 8.0),
+                   Text('Open Everyday'),
+               ],
+               ),
+               Column(
+                 children: const <Widget>[
+                   Icon(Icons.access_time),
+                   SizedBox(height: 8.0),
+                   Text('09:00 - 20:00'),
+                 ],
+               ),
+               Column(
+                 children: const <Widget>[
+                   Icon(Icons.attach_money_outlined),
+                   SizedBox(height: 8.0),
+                   Text('RP 25.000'),
+                 ],
+               )
             ],
-          )
-
-        ],
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
+           )
+          ),
+         Container(
+           padding: const EdgeInsets.all(16.0),
+           child: const Text(
+             'Malino Highlands adalah destinasi wisata pegunungan yang memukau di Sulawesi Selatan, terkenal dengan pemandangan hijau yang membentang luas, kebun teh yang asri, serta udara sejuk yang menyegarkan. Terletak di dataran tinggi Gowa, tempat ini menawarkan pengalaman unik dengan panorama alam yang memanjakan mata, lengkap dengan spot foto yang instagramable. Pengunjung bisa menikmati berjalan-jalan di antara perkebunan teh, menghirup udara segar, atau bersantai di kafe sambil menikmati teh lokal. Malino Highlands juga memiliki taman bunga yang indah dan berbagai fasilitas rekreasi yang cocok untuk wisata keluarga atau liburan romantis.',
+             textAlign: TextAlign.center,
+             style: TextStyle(fontSize:16.0),
+           ),
+         ),
+         ],
+        ),
       ),
     );
   }
