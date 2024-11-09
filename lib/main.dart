deef244a4339b57949b17c5ae96672c850ccb07d
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-09 23:08:32 +0800

    Belajar : Input Widget

--- lib/main.dart
+++ lib/main.dart
@@ -142,6 +142,16 @@ class FirstScreen extends StatefulWidget {
 
 class _FirstScreenState extends State<FirstScreen> {
   String? language;
+  String _name = '';
+  bool lighton = false;
+  bool agree = false;
+  TextEditingController _controller = TextEditingController();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -150,30 +160,146 @@ class _FirstScreenState extends State<FirstScreen> {
         title: const Text('First Screen'),
       ),
       body: Center(
-        child: DropdownButton<String>(
-          items: const <DropdownMenuItem<String>>[
-            DropdownMenuItem<String>(
-              value: 'Dart',
-              child: Text('Dart'),
+        child: Column (
+          children: [
+             DropdownButton<String>(
+              items: const <DropdownMenuItem<String>>[
+                DropdownMenuItem<String>(
+                  value: 'Dart',
+                  child: Text('Dart'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'Kotlin',
+                  child: Text('Kotlin'),
+                ),
+                DropdownMenuItem<String>(
+                  value: 'Swift',
+                  child: Text('Swift'),
+                ),
+              ],
+              value: language,
+              hint: const Text('Select Language'),
+              onChanged: (String? value) {
+                setState(() {
+                  language = value;
+                });
+              },
             ),
-            DropdownMenuItem<String>(
-              value: 'Kotlin',
-              child: Text('Kotlin'),
-            ),
-            DropdownMenuItem<String>(
-              value: 'Swift',
-              child: Text('Swift'),
+             Padding(
+              padding: const EdgeInsets.all(24.0), // Ubah nilai sesuai kebutuhan
+              child: Column(
+                children: [
+                TextField(
+                  // onChanged: (String value) { // Parameter onChanged berisi sebuah fungsi yang akan dipanggil setiap terjadi perubahan inputan pada TextField.
+                    decoration: const InputDecoration(
+                      hintText: 'Write your name here...',
+                      labelText: 'Your Name',
+                     ),
+                    onChanged: (String value){
+                      setState(() {
+                        _name = value;
+                      });
+                    },
+                ),
+                const SizedBox(height: 20,),
+                ElevatedButton(
+                    child: const Text('Submit'),
+                    onPressed: (){
+                      showDialog(
+                          context: context,
+                          builder: (context){
+                            return AlertDialog(
+                              content: Text('Hello, $_name'),
+                            );
+                          });
+                       },
+                ),
+                Switch(
+                    value: lighton,
+                    onChanged: (bool value){
+                      setState(() {
+                        lighton = value;
+                      });
+
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text(lighton? 'light on' : 'light off'),
+                            duration: Duration(seconds: 1),
+                          ),
+                      );
+                    }
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    ListTile(
+                      leading: Radio<String>(
+                          value: 'Dart', 
+                          groupValue: language, 
+                          onChanged: (String? value){
+                            setState(() {
+                              language = value;
+                              showSnackbar();
+                            });
+                          },
+                      ),
+                      title: Text('Dart'),
+                    ),
+                    ListTile(
+                      leading: Radio<String>(
+                        value: 'Kotlin',
+                        groupValue: language,
+                        onChanged: (String? value){
+                          setState(() {
+                            language = value;
+                            showSnackbar();
+                          });
+                        },
+                      ),
+                      title: Text('Kotlin'),
+                    ),
+                    ListTile(
+                      leading: Radio<String>(
+                        value: 'Swift',
+                        groupValue: language,
+                        onChanged: (String? value){
+                          setState(() {
+                            language = value;
+                            showSnackbar();
+                          });
+                        },
+                      ),
+                      title: Text('Swift'),
+                    ),
+                  ],
+                ),
+                ListTile(
+                  leading: Checkbox(
+                      value: agree, 
+                      onChanged: (bool? value) {
+                        setState(() {
+                          agree = value!;
+                        });
+                      },
+                  ),
+                  title: Text('Agree / Disagree'),
+                ),
+                ],
+              ),
             ),
           ],
-          value: language,
-          hint: const Text('Select Language'),
-          onChanged: (String? value) {
-            setState(() {
-              language = value;
-            });
-          },
         ),
       ),
     );
   }
+  void showSnackbar() {
+    ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+    content: Text('$language selected'),
+    duration: Duration(seconds: 1) ,
+    ),
+    );
+  }
 }
+
+
