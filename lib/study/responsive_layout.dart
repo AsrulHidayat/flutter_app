03eae8208bbac1f00e2bffaf7c53e8179452676e
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-13 13:19:09 +0800

    Belajar : Responsive Layout - 3 Model Tampilan berdasarkan ukuran layar

--- lib/study/responsive_layout.dart
+++ lib/study/responsive_layout.dart
@@ -30,6 +30,16 @@ class HomePage extends StatelessWidget {
                       style: const TextStyle(color: Colors.white, fontSize: 18),
                       textAlign: TextAlign.center,
                     ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const ResponsivePage()),
+                        );
+                      },
+                      child: const Text('Go to Responsive Page'),
+                    ),
                   ],
                 );
               },
@@ -64,5 +74,47 @@ class HomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+
+class ResponsivePage extends StatelessWidget {
+  const ResponsivePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return ListView(
+              children: _generateContainers(),
+            );
+          } else if (constraints.maxWidth < 900) {
+            return GridView.count(
+              crossAxisCount: 2,
+              children: _generateContainers(),
+            );
+          } else {
+            return GridView.count(
+              crossAxisCount: 6,
+              children: _generateContainers(),
+            );
+          }
+        },
+      ),
+
+    );
+  }
+
+  List<Widget> _generateContainers() {
+    return List<Widget>.generate(20, (index) {
+      return Container(
+        margin: const EdgeInsets.all(8),
+        color: Colors.blueGrey,
+        height: 200,
+      );
+    });
   }
 }
