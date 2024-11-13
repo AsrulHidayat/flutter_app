bdef6aaa1505476eb8acb0e5dc43a96a38b951e1
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-13 11:20:41 +0800

    Belajar : Responsive Layout

--- lib/wisatagowa/detail_screen.dart
+++ lib/wisatagowa/detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
 import 'package:flutter_app/study/expanded_and_flexible.dart';
 import 'package:flutter_app/study/navigation.dart';
+import 'package:flutter_app/study/responsive_layout.dart';
 
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
@@ -156,30 +157,67 @@ class DetailScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const Text(
-                      'Tes',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 16), // Jarak 8 piksel di bawah teks
+                      child: const Text(
+                        'Link Other Study',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                     Align(
                       alignment: Alignment.centerLeft,
-                      child: TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => ThirdScreen()),
-                          );
-                        },
-                        child: const Text(
-                          '1. Navigation',
-                          style: TextStyle(
-                          fontSize: 16.0,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8), // Jarak 8 piksel di samping kiri
+                          child: Wrap(
+                            direction: Axis.vertical, // Mengatur agar tombol-tombol disusun secara vertikal
+                            spacing: 8, // Jarak vertikal antar tombol
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => ThirdScreen()),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero, // Hilangkan padding di sekitar teks
+                                  minimumSize: Size(0, 0), // Ukuran minimum 0, agar mengikuti ukuran teks
+                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Mengurangi area sentuh ke ukuran teks
+                                ),
+                                child: const Text(
+                                  '1. Navigation',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => HomePage()),
+                                  );
+                                },
+                                style: TextButton.styleFrom(
+                                  padding: EdgeInsets.zero, // Hilangkan padding di sekitar teks
+                                  minimumSize: Size(0, 0), // Ukuran minimum 0, agar mengikuti ukuran teks
+                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Mengurangi area sentuh ke ukuran teks
+                                ),
+                                child: const Text(
+                                  '2. Responsive Layout',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ),
+                        )
+
+                    )
                   ],
                 ),
               ),
