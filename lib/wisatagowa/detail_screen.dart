66ffc00356d8161e992586197eb12256abf16119
Asrul Hidayat <136725199+AsrulHidayat@users.noreply.github.com>
2024-11-14 11:23:43 +0800

    Belajar : Pengembangan untuk Web - Detail Halaman
    Kesalahan bagian Halaman, mengalami perulangan tampilan sebanyak 4 kali

--- lib/wisatagowa/detail_screen.dart
+++ lib/wisatagowa/detail_screen.dart
@@ -2,16 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/main.dart';
 import 'package:flutter_app/model/tourism_place.dart';
 import 'package:flutter_app/study/expanded_and_flexible.dart';
-import 'package:flutter_app/study/navigation.dart';
-import 'package:flutter_app/study/responsive_layout.dart';
 
 class DetailScreen extends StatelessWidget {
   final TourismPlace place;
+
   const DetailScreen({Key? key, required this.place}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    var informationTextStyle = const TextStyle(fontFamily: 'Oxygen');
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebPage(place: place);
+        } else {
+          return DetailMobilePage(place: place);
+        }
+      },
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  final TourismPlace place;
+
+  const DetailMobilePage({Key? key, required this.place}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
@@ -63,7 +80,6 @@ class DetailScreen extends StatelessWidget {
                       const SizedBox(height: 8.0),
                       Text(
                         place.openDays,
-                        style: informationTextStyle,
                       ),
                     ],
                   ),
@@ -73,7 +89,6 @@ class DetailScreen extends StatelessWidget {
                       const SizedBox(height: 8.0),
                       Text(
                         place.openTime,
-                        style: informationTextStyle,
                       ),
                     ],
                   ),
@@ -83,7 +98,6 @@ class DetailScreen extends StatelessWidget {
                       const SizedBox(height: 8.0),
                       Text(
                         place.ticketPrice,
-                        style: informationTextStyle,
                       ),
                     ],
                   ),
@@ -119,8 +133,8 @@ class DetailScreen extends StatelessWidget {
                             child: Image.network(url),
                           ),
                         );
-                        }).toList(),
-                   ),
+                      }).toList(),
+                    ),
                   ),
                 ],
               ),
@@ -147,106 +161,198 @@ class DetailScreen extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const Rainbow()),
-                          );
-                        },
-                        child: const Text('Expanded'),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
                       ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Rainbow()),
+                        );
+                      },
+                      child: const Text('Expanded'),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
+            ),
             const Divider(
               color: Colors.grey,
               thickness: 1.0,
-            ),
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 16), // Jarak 8 piksel di bawah teks
-                      child: const Text(
-                        'Link Other Study',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 8), // Jarak 8 piksel di samping kiri
-                          child: Wrap(
-                            direction: Axis.vertical, // Mengatur agar tombol-tombol disusun secara vertikal
-                            spacing: 8, // Jarak vertikal antar tombol
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => ThirdScreen()),
-                                  );
-                                },
-                                style: TextButton.styleFrom(
-                                  padding: EdgeInsets.zero, // Hilangkan padding di sekitar teks
-                                  minimumSize: Size(0, 0), // Ukuran minimum 0, agar mengikuti ukuran teks
-                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Mengurangi area sentuh ke ukuran teks
-                                ),
-                                child: const Text(
-                                  '1. Navigation',
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                              ),
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => HomePage()),
-                                  );
-                                },
-                                style: TextButton.styleFrom(
-                                  padding: EdgeInsets.zero, // Hilangkan padding di sekitar teks
-                                  minimumSize: Size(0, 0), // Ukuran minimum 0, agar mengikuti ukuran teks
-                                  tapTargetSize: MaterialTapTargetSize.shrinkWrap, // Mengurangi area sentuh ke ukuran teks
-                                ),
-                                child: const Text(
-                                  '2. Responsive Layout',
-                                  style: TextStyle(
-                                    fontSize: 16.0,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-
-                    )
-                  ],
-                ),
-              ),
             ),
           ],
         ),
       ),
     );
   }
-  }class FavoriteButton extends StatefulWidget {
+}
+
+class DetailWebPage extends StatefulWidget {
+  final TourismPlace place;
+
+  const DetailWebPage({Key? key, required this.place}) : super (key: key);
+
+  @override
+  State<DetailWebPage> createState() => _DetailWebPageState();
+}
+
+class _DetailWebPageState extends State<DetailWebPage> {
+  final _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Scrollbar(
+        controller: _scrollController,
+        child: Container(
+          height: 1500,
+            padding: const EdgeInsets.only(bottom: 16),
+            child: ListView (
+              controller: _scrollController,
+              scrollDirection: Axis.vertical,
+              children: widget.place.imageUrls.map((url) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                    horizontal: 64,
+                  ),
+                  child: Center (
+                    child: SizedBox(
+                      width: 1200,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Wisata Bandung',
+                            style: TextStyle(
+                              fontFamily: 'Staatliches',
+                              fontSize: 32,
+                            ),
+                          ),
+                          const SizedBox(height: 32),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    ClipRRect(
+                                      child: Image.asset(widget.place.imageAsset),
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                    const SizedBox(height: 16),
+                                    Container(
+                                      height: 150,
+                                      padding: const EdgeInsets.only(bottom: 16),
+                                      child: ListView(
+                                        scrollDirection: Axis.horizontal,
+                                        children: widget.place.imageUrls.map((url) {
+                                          return Padding(
+                                            padding: const EdgeInsets.all(4.0),
+                                            child: ClipRRect(
+                                              borderRadius: BorderRadius.circular(10),
+                                              child: Image.network(url),
+                                            ),
+                                          );
+                                        }).toList(),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(width: 32),
+                              Expanded(
+                                child: Card(
+                                  child: Container(
+                                    padding: const EdgeInsets.all(16),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.max,
+                                      children: <Widget>[
+                                        Container(
+                                          child: Text(
+                                            widget.place.name,
+                                            textAlign: TextAlign.center,
+                                            style: const TextStyle(
+                                              fontSize: 30.0,
+                                              fontFamily: 'Staatliches',
+                                            ),
+                                          ),
+                                        ),
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Row(
+                                              children: <Widget>[
+                                                const Icon(Icons.calendar_today),
+                                                const SizedBox(width: 8.0),
+                                                Text(
+                                                  widget.place.openDays,
+                                                ),
+                                              ],
+                                            ),
+                                            const FavoriteButton(),
+                                          ],
+                                        ),
+                                        Row(
+                                          children: <Widget>[
+                                            const Icon(Icons.access_time),
+                                            const SizedBox(width: 8.0),
+                                            Text(
+                                              widget.place.openTime,
+                                            ),
+                                          ],
+                                        ),
+                                        const SizedBox(height: 8.0),
+                                        Row(
+                                          children: <Widget>[
+                                            const Icon(Icons.monetization_on),
+                                            const SizedBox(width: 8.0),
+                                            Text(
+                                              widget.place.ticketPrice,
+                                            ),
+                                          ],
+                                        ),
+                                        Container(
+                                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                          child: Text(
+                                            widget.place.description,
+                                            textAlign: TextAlign.justify,
+                                            style: const TextStyle(
+                                              fontSize: 16.0,
+                                              fontFamily: 'Oxygen',
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+        ),
+      ),
+
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
   const FavoriteButton({Key? key}) : super(key: key);
 
   @override
